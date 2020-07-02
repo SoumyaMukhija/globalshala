15d4cdfba00429999b33f7d0bf62faf3394976e6
Soumya Mukhija <soumyamukhija@gmail.com>
2020-07-03 00:49:56 +0530

    merged

--- src/utils/network_utils.js
+++ src/utils/network_utils.js
@@ -1,5 +1,4 @@
-
-const BASE_URL = "https://81e2aa776c33.ngrok.io"
+const BASE_URL = "https://81e2aa776c33.ngrok.io";
 
 export async function makeRequest() {
   const response = await fetch(BASE_URL);
@@ -8,11 +7,7 @@ export async function makeRequest() {
 }
 
 export async function makeProgressRequest() {
-<<<<<<< HEAD
-  const response = await fetch("https://29f57a9e09fa.ngrok.io/progress"); //link serving progress data
-=======
-  const response = await fetch(BASE_URL+"/progress/");
->>>>>>> 3f75c84185d50c0b6e4f0bf0e2cc41aa9c3609ec
+  const response = await fetch(BASE_URL + "/progress/");
   const json = await response.json();
-  return json
+  return json;
 }
