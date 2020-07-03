8487ee43c2e6f490b38de99c9cad58bd1a9bd7fa
sumit mukhija <sumitmukhija@hotmail.com>
2020-07-03 01:55:34 +0100

    Add inapp browser and navigation to chrome

--- src/screens/ProgressPage.js
+++ src/screens/ProgressPage.js
@@ -5,12 +5,29 @@ import { Text, View } from "react-native";
 import ScreenStyles, {TextStyle} from "./CommonStyles"
 import Progress from "../components/Progress"
 import Loading from "./LoadingPage"
+import AppModal from "../components/Modal";
 
 export default function ProgressPage() {
 
   const [data, setData] = useState(undefined)
+  const [modalShown, setModalShown] = useState(true)
+  const [currentSelectedUrl, setCurrentSelectedUrl] = useState(true)
+
+  function openUrlInternally(url){
+    if(url){
+      console.log(url);
+      setCurrentSelectedUrl(url)
+      setModalShown(true)
+    }
+  }
+
+  function closeModal(){
+    setModalShown(false)
+  }
 
   const showActionSheet = (item) => {
+    console.log(item);
+    
     let CONTENT = [{ text: " "},{ text: item["Description"] || "No Description"},{ text: " "},{ text: "More About this", icon: "book", iconColor: "#048FC1" }];
     ActionSheet.show(
                 {
@@ -19,7 +36,8 @@ export default function ProgressPage() {
                 },
                 buttonIndex => {
                   if(buttonIndex == 3){
-                    openInBrowser(item['Related'][0])
+                    
+                    openUrlInternally(item['Related'][0])
                   }
                 }
               )
@@ -32,23 +50,29 @@ export default function ProgressPage() {
 
   useEffect(() => {
     getProgressSteps()
+    setModalShown(false)
   }, []);
+
+  
 
   if(data){
     return (
       <Root>
+      <AppModal visible={modalShown} url={currentSelectedUrl} closeModal={closeModal}/>
       <View style = {ScreenStyles.screen}>
         <Text style={TextStyle.heading}>Map your journey</Text>
         <Text style={TextStyle.subtitle}>with Globalshala!</Text>
         <Progress data={data} showActionSheet={showActionSheet}/>
-        <Fab
+        {/* <Fab
             style={{ backgroundColor: '#048FC1', marginLeft:60}}
             position="bottomLeft"
             containerStyle={{left:'50%'}}
             onPress={() => {alert('Chat')}}>
             <Icon name="chatbubbles" />
-          </Fab>
+          </Fab> */}
+          
       </View>
+      
       </Root>
     );
   }else{
